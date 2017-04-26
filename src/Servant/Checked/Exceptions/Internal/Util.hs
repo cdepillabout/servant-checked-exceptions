17c2f39dbccf6d9bcd599788945b94b3bad35d1b
(cdep)illabout <cdep.illabout@gmail.com>
2017-04-27 00:41:42 +0900

    Add documenation for Util.

--- src/Servant/Checked/Exceptions/Internal/Util.hs
+++ src/Servant/Checked/Exceptions/Internal/Util.hs
@@ -3,8 +3,33 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 
+{- |
+Module      :  Servant.Checked.Exceptions.Internal.Util
+
+Copyright   :  Dennis Gosnell 2017
+License     :  BSD3
+
+Maintainer  :  Dennis Gosnell (cdep.illabout@gmail.com)
+Stability   :  experimental
+Portability :  unknown
+
+Additional helpers.
+-}
+
 module Servant.Checked.Exceptions.Internal.Util where
 
+-- | A type-level @snoc@.
+--
+-- Append to an empty list:
+--
+-- >>> import Data.Type.Equality ((:~:)(Refl))
+-- >>> Refl :: Snoc '[] Double :~: '[Double]
+-- Refl
+--
+-- Append to a non-empty list:
+--
+-- >>> Refl :: Snoc '[Char] String :~: '[Char, String]
+-- Refl
 type family Snoc (as :: [k]) (b :: k) where
   Snoc '[] b = '[b]
   Snoc (a ': as) b = (a ': Snoc as b)
