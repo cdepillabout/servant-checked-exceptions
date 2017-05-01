c9ea19b10599b799b5fb82c72ad8a78509862293
(cdep)illabout <cdep.illabout@gmail.com>
2017-05-01 14:26:39 +0900

    Add documentation for erver example.

--- example/Server.hs
+++ example/Server.hs
@@ -30,9 +30,25 @@ import Api
         IncorrectCapitalization(IncorrectCapitalization),
         SearchQuery(SearchQuery), SearchResponse, port)
 
+-- | This is our server root for the 'ServerT' for 'Api'.  We only have two
+-- handlers, 'postStrictSearch' and 'postLaxSearch'.
 serverRoot :: ServerT Api Handler
 serverRoot = postStrictSearch :<|> postLaxSearch
 
+-- | This is the handler for 'Api.ApiStrictSearch'.
+--
+-- If we get the 'SearchQuery' @\"Hello\"@, we return a 'SuccEnvelope'.
+-- However, if we get a search query like @\"hello\"@, we return an
+-- 'ErrEnvelope' with an 'IncorrectCapitialization' error.  If we get a search
+-- query that is not @\"hello\"@, we return an 'ErrEnvelope' with a
+-- 'BadSearchTermErr'.
+--
+-- Notice how we can use the polymorphic function 'pureErrEnvelope' to return
+-- either an 'IncorrectCapitialization' error, or a 'BadSearchTermErr', even
+-- though these two have different types.
+--
+-- Also, notice how this function returns an 'Envelope' because we are using
+-- 'Throws' in the api definition.
 postStrictSearch
   :: SearchQuery
   -> Handler (Envelope '[BadSearchTermErr, IncorrectCapitalization] SearchResponse)
@@ -41,6 +57,10 @@ postStrictSearch (SearchQuery query)
   | fmap toLower query == "hello" = pureErrEnvelope IncorrectCapitalization
   | otherwise = pureErrEnvelope BadSearchTermErr
 
+-- | This is the handler for 'Api.ApiLaxSearch'.
+--
+-- This is similar to 'postStrictSearch', but it doesn't require correctly
+-- capitalization.
 postLaxSearch
   :: SearchQuery
   -> Handler (Envelope '[BadSearchTermErr] SearchResponse)
@@ -48,8 +68,10 @@ postLaxSearch (SearchQuery query)
   | fmap toLower query == "hello" = pureSuccEnvelope "good"
   | otherwise = pureErrEnvelope BadSearchTermErr
 
+-- | Create a WAI 'Application'.
 app :: Application
 app = serve (Proxy :: Proxy Api) serverRoot
 
+-- | Run the WAI 'Application' using 'run' on the port defined by 'port'.
 main :: IO ()
 main = run port app
