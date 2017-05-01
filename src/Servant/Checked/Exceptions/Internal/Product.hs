322229a8ef255f11f809c685201405a4f70c6abf
(cdep)illabout <cdep.illabout@gmail.com>
2017-05-01 13:38:18 +0900

    Add some documentation to the instances for Product.

--- src/Servant/Checked/Exceptions/Internal/Product.hs
+++ src/Servant/Checked/Exceptions/Internal/Product.hs
@@ -49,20 +49,25 @@ data Product (f :: u -> *) (as :: [u]) where
 
 -- | This type class provides a way to turn a tuple into a 'Product'.
 class ToProduct (tuple :: *) (f :: u -> *) (as :: [u]) | f as -> tuple where
+  -- | Convert a tuple into a 'Product'.  See 'tupleToProduct' for examples.
   toProduct :: tuple -> Product f as
 
+-- | Convert a single value into a 'Product'.
 instance forall (f :: u -> *) (a :: u). ToProduct (f a) f '[a] where
   toProduct :: f a -> Product f '[a]
   toProduct fa = Cons fa Nil
 
+-- | Convert a tuple into a 'Product'.
 instance forall (f :: u -> *) (a :: u) (b :: u). ToProduct (f a, f b) f '[a, b] where
   toProduct :: (f a, f b) -> Product f '[a, b]
   toProduct (fa, fb) = Cons fa $ Cons fb Nil
 
+-- | Convert a 3-tuple into a 'Product'.
 instance forall (f :: u -> *) (a :: u) (b :: u) (c :: u). ToProduct (f a, f b, f c) f '[a, b, c] where
   toProduct :: (f a, f b, f c) -> Product f '[a, b, c]
   toProduct (fa, fb, fc) = Cons fa $ Cons fb $ Cons fc Nil
 
+-- | Convert a 4-tuple into a 'Product'.
 instance forall (f :: u -> *) (a :: u) (b :: u) (c :: u) (d :: u). ToProduct (f a, f b, f c, f d) f '[a, b, c, d] where
   toProduct :: (f a, f b, f c, f d) -> Product f '[a, b, c, d]
   toProduct (fa, fb, fc, fd) = Cons fa $ Cons fb $ Cons fc $ Cons fd Nil
@@ -86,21 +91,25 @@ type OpenProduct = Product Identity
 class ToOpenProduct (tuple :: *) (as :: [*]) | as -> tuple where
   toOpenProduct :: tuple -> OpenProduct as
 
+-- | Convert a single value into an 'OpenProduct'.
 instance forall (a :: *). ToOpenProduct a '[a] where
   toOpenProduct :: a -> OpenProduct '[a]
   toOpenProduct a = Cons (Identity a) Nil
 
+-- | Convert a tuple into an 'OpenProduct'.
 instance
     forall (a :: *) (b :: *). ToOpenProduct (a, b) '[a, b] where
   toOpenProduct :: (a, b) -> OpenProduct '[a, b]
   toOpenProduct (a, b) = Cons (Identity a) $ Cons (Identity b) Nil
 
+-- | Convert a 3-tuple into an 'OpenProduct'.
 instance
     forall (a :: *) (b :: *) (c :: *). ToOpenProduct (a, b, c) '[a, b, c] where
   toOpenProduct :: (a, b, c) -> OpenProduct '[a, b, c]
   toOpenProduct (a, b, c) =
     Cons (Identity a) $ Cons (Identity b) $ Cons (Identity c) Nil
 
+-- | Convert a 4-tuple into an 'OpenProduct'.
 instance
     forall (a :: *) (b :: *) (c :: *) (d :: *).
     ToOpenProduct (a, b, c, d) '[a, b, c, d] where
@@ -129,10 +138,12 @@ tupleToOpenProduct = toOpenProduct
 -- Instances --
 ---------------
 
+-- | Show 'Nil' values.
 instance Show (Product f '[]) where
   show :: Product f '[] -> String
   show Nil = "Nil"
 
+-- | Show 'Cons' values.
 instance (Show (f a), Show (Product f as)) => Show (Product f (a ': as)) where
   showsPrec :: Int -> (Product f (a ': as)) -> String -> String
   showsPrec n (Cons fa prod) = showParen (n > 10) $
